14b650f11f50d71174580d345ff284bb6540b73d
ChernyaevaMargarita <chernyaeva760@yandex.by>
2021-02-05 17:46:51 +0300

    easy model with only 1 person need to do: - second VC - actual model - fix constraints - load images - MVP

--- User Info/Model/User.swift
+++ User Info/Model/User.swift
@@ -1,17 +1,19 @@
 //
-//  UserInfoModel.swift
-//  User Info
+//  User.swift
+//  User
 //
 //  Created by Маргарита Черняева on 2/5/21.
 //
 
 import Foundation
 
-struct User {
-    var name: String
-    var avatar: String?
-    var email: String?
-    var followersCount: Int?
-    var followingCount: Int?
-    var date: String?
+struct User: Codable {
+    let avatar_url : String?
+    let name : String?
+    let email : String?
+    let followers : Int?
+    let following : Int?
+    let created_at : String?
 }
+
+
--- User Info/ViewController/ViewController.swift
+++ User Info/ViewController/ViewController.swift
@@ -10,18 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     var users = [User]()
+    var margaritasURL = "https://api.github.com/users/margaritachernyaeva"
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-        users.append(User(name: "Nina", email: "ninaHaha@mail.ru", followersCount: 125, followingCount: 4))
+        getData(from: margaritasURL)
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        print(users.count)
+    }
     
    private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    private func getData(from url: String) {
+        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let data = data, let _ = response else { return }
+            var result: User?
+            do {
+                result = try JSONDecoder().decode(User.self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+            guard let json = result else { return }
+            self.users.append(json)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }.resume()
     }
 }
 
@@ -32,7 +56,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-        cell.nameLabel.text = users[indexPath.row].name
+        let user = users[indexPath.row]
+        // " " чтобы ничего не плыло, если значение nil
+        cell.nameLabel.text = user.name ?? " "
+        cell.emailLabel.text = user.email ?? " "
+        if let followers = user.followers {
+            cell.followersLabel.text = String(followers)
+        } else {
+            cell.followersLabel.text = " "
+        }
+        if let following = user.following {
+            cell.followingLabel.text = String(following)
+        } else {
+            cell.followingLabel.text = " "
+        }
+        // тут приводим дату к читаемому виду
+        cell.dateLabel.text = String((user.created_at ?? "").dropLast(10))
         return cell
     }
     
